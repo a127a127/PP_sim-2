33bc6442ebddda4a574a9640c920e661ef7c90a9
eclab <eclab@eclabde-MacBook-Pro.local>
2019-07-15 20:16:53 +0800

    Caffenet

---
+++ CaffenetConfig.py
@@ -0,0 +1,26 @@
+from LayerMetaData import LayerMetaData
+
+class CaffenetConfig(object):
+    def __init__(self):
+        #layer data
+        self.layer_list = [
+            LayerMetaData("convolution", 96, 11, 11, 3, 0, 0, 0), # (type, conv_n, conv_h, conv_w, conv_c, pool_h, pool_w, fully_n)
+            LayerMetaData("convolution", 256, 5, 5, 96, 0, 0, 0),
+            LayerMetaData("convolution", 348, 3, 3, 256, 0, 0, 0),
+            LayerMetaData("convolution", 348, 3, 3, 348, 0, 0, 0),
+            LayerMetaData("convolution", 256, 3, 3, 348, 0, 0, 0),
+            LayerMetaData("fully",      0, 0, 0, 0, 0, 0, 4096),
+            LayerMetaData("fully",      0, 0, 0, 0, 0, 0, 4096),
+            LayerMetaData("fully",      0, 0, 0, 0, 0, 0, 10)
+            ]
+
+
+        #model data
+        self.input_n = 1
+        self.input_h = 224
+        self.input_w = 224
+        self.input_c = 3
+        
+        #bit decomposition
+        self.input_bit = 16
+        self.filter_bit = 16
