563f01607da7f0a53b7b003d607a3d1f78bfb6ae
wei-ting <r06944031@ntu.edu.tw>
2019-09-18 17:57:18 +0800

    del ModelMetaData.py; Check: Model.py, ModelConfig.py, LayeraMetaData.py

--- LayerMetaData.py
+++ LayerMetaData.py
@@ -1,22 +1,13 @@
 class LayerMetaData(object):
     def __init__(self, layer_type, filter_n, filter_h, filter_w, filter_c, pooling_h, pooling_w, neuron_n):
-        self.layer_type = layer_type # "convolution", "pooling"
-
-        # for convolution layer information
+        self.layer_type = layer_type # "convolution", "pooling", "fully"
         self.filter_n = filter_n
         self.filter_h = filter_h
         self.filter_w = filter_w
         self.filter_c = filter_c
-        self.filter_length = self.filter_h * self.filter_w * self.filter_c
-
-        # for pooling layer information
         self.pooling_h = pooling_h
         self.pooling_w = pooling_w
-
-        # for fully connected / output layer information
         self.neuron_n = neuron_n
-
-
 
     def __str__(self):
         return str(self.__dict__)
--- Model.py
+++ Model.py
@@ -2,19 +2,18 @@ class Model(object):
     def __init__(self, model_config):
         self.layer_list = model_config.layer_list 
         self.layer_length = len(self.layer_list)
-        self.input_n = model_config.input_n
-        self.input_h = [model_config.input_h] 
-        self.input_w = [model_config.input_w] 
-        self.input_c = [model_config.input_c] 
-        self.input_number = [] # input windows number
         self.filter_n = [] 
         self.filter_h = []
         self.filter_w = []
         self.filter_c = []
         self.filter_length = []
+        self.input_n = model_config.input_n
+        self.input_h = [model_config.input_h] 
+        self.input_w = [model_config.input_w] 
+        self.input_c = [model_config.input_c] 
+        self.input_number = []
         self.pooling_h = []
         self.pooling_w = []
-
         self.input_bit = model_config.input_bit
         self.filter_bit = model_config.filter_bit
 
@@ -24,37 +23,44 @@ class Model(object):
                 self.filter_h.append(self.layer_list[nlayer].filter_h)
                 self.filter_w.append(self.layer_list[nlayer].filter_w)
                 self.filter_c.append(self.layer_list[nlayer].filter_c)
-                self.filter_length.append(self.layer_list[nlayer].filter_h * self.layer_list[nlayer].filter_w * self.layer_list[nlayer].filter_c)
+                self.filter_length.append(self.layer_list[nlayer].filter_h * \
+                                          self.layer_list[nlayer].filter_w * \
+                                          self.layer_list[nlayer].filter_c)
+                self.input_h.append(self.input_h[nlayer] - self.layer_list[nlayer].filter_h + 1) # stride = 1, no padding
+                self.input_w.append(self.input_w[nlayer] - self.layer_list[nlayer].filter_w + 1) # stride = 1, no padding
+                self.input_c.append(self.layer_list[nlayer].filter_n)
+                self.input_number.append((self.input_h[nlayer] - self.layer_list[nlayer].filter_h + 1) * \
+                                         (self.input_w[nlayer] - self.layer_list[nlayer].filter_w + 1)) # stride = 1, no padding
                 self.pooling_h.append(0)
                 self.pooling_w.append(0)
-                self.input_h.append(self.input_h[nlayer] - self.layer_list[nlayer].filter_h + 1) # stride = 1
-                self.input_w.append(self.input_w[nlayer] - self.layer_list[nlayer].filter_w + 1) # stride = 1
-                self.input_c.append(self.layer_list[nlayer].filter_n)
-                self.input_number.append((self.input_h[nlayer] - self.layer_list[nlayer].filter_h + 1) * (self.input_w[nlayer] - self.layer_list[nlayer].filter_w + 1))
+
             elif self.layer_list[nlayer].layer_type == "pooling":
                 self.filter_n.append(0)
                 self.filter_h.append(0)
                 self.filter_w.append(0)
                 self.filter_c.append(0)
                 self.filter_length.append(0)
+                self.input_h.append(self.input_h[nlayer] // self.layer_list[nlayer].pooling_h) # no padding
+                self.input_w.append(self.input_w[nlayer] // self.layer_list[nlayer].pooling_w) # no padding
+                self.input_c.append(self.input_c[nlayer])
+                self.input_number.append((self.input_h[nlayer] // self.layer_list[nlayer].pooling_h) * \
+                                         (self.input_w[nlayer] // self.layer_list[nlayer].pooling_w) * \
+                                         (self.input_c[nlayer])) # no padding
                 self.pooling_h.append(self.layer_list[nlayer].pooling_h)
                 self.pooling_w.append(self.layer_list[nlayer].pooling_w)
-                self.input_h.append(self.input_h[nlayer] // self.layer_list[nlayer].pooling_h)
-                self.input_w.append(self.input_w[nlayer] // self.layer_list[nlayer].pooling_w)
-                self.input_c.append(self.input_c[nlayer])
-                self.input_number.append((self.input_h[nlayer] // self.layer_list[nlayer].pooling_h) * (self.input_w[nlayer] // self.layer_list[nlayer].pooling_w) * (self.input_c[nlayer]))
+
             elif self.layer_list[nlayer].layer_type == "fully":
                 self.filter_n.append(self.layer_list[nlayer].neuron_n)
-                self.filter_h.append(0)
-                self.filter_w.append(0)
-                self.filter_c.append(0)
+                self.filter_h.append(1)
+                self.filter_w.append(1)
+                self.filter_c.append(1)
                 self.filter_length.append(self.input_h[nlayer] * self.input_w[nlayer] * self.input_c[nlayer])
+                self.input_h.append(self.layer_list[nlayer].neuron_n)
+                self.input_w.append(1)
+                self.input_c.append(1)
+                self.input_number.append(1)
                 self.pooling_h.append(0)
                 self.pooling_w.append(0)
-                self.input_h.append(self.layer_list[nlayer].neuron_n)
-                self.input_w.append(0)
-                self.input_c.append(0)
-                self.input_number.append(self.layer_list[nlayer].neuron_n)
 
     def __str__(self):
         return str(self.__dict__)
