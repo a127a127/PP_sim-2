07d2867153fe20181485ba9dcf9a16634eed21d1
wei-ting <r06944031@ntu.edu.tw>
2019-12-04 12:34:43 +0800

    slot

--- EventMetaData.py
+++ EventMetaData.py
@@ -1,4 +1,8 @@
 class EventMetaData:
+    __slots__ = ["event_type", "preceding_event_count", "current_number_of_preceding_event",
+                 "proceeding_event", "nlayer", "inputs", "outputs", 
+                 "position_idx", "finished", "data_is_transfer"
+                ]
     def __init__(self, event_type, position_idx, preceding_event_count, proceeding_event, nlayer, inputs, outputs):
         
         self.event_type = event_type  # edram_rd_ir, ou, cu_saa, pe_saa, activation, edram_wr, edram_rd_pool, pooling, data_transfer
