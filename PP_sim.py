f9380250cf38a53ce321d8960220f0cc1721707e
wei-ting <r06944031@ntu.edu.tw>
2020-03-03 18:31:31 +0800

    整理code

--- PP_sim.py
+++ PP_sim.py
@@ -3,18 +3,16 @@
 from Mapping import SameColumnFirstMapping
 from Mapping import SameRowFirstMapping
 from Mapping import ParallelsimMapping
-
 from OrderGenerator import OrderGenerator
 from Controller import Controller
 
 import time, sys, os
-import pickle
+#import pickle
 
 def main():
     start_time = time.time()
     mapping = int(sys.argv[1])
     scheduling = int(sys.argv[2])
-    paral = int(sys.argv[3])
 
     ### Mapping ##
     start_mapping_time = time.time()
@@ -40,16 +38,11 @@ def main():
         mapping_str = "Same_Row_First_Mapping"
     elif mapping == 4: # ParallelsimMapping
         print("Parallelsim Mapping"+sys.argv[3])
-        mapping_information = ParallelsimMapping(paral)
+        mapping_information = ParallelsimMapping(int(sys.argv[3]))
         mapping_str = "Parallelsim_Mapping"+sys.argv[3]
 
     end_mapping_time = time.time()
     print("--- Mapping is finished in %s seconds ---\n" % (end_mapping_time - start_mapping_time))
-
-    # lp = mapping_information.layer_mapping_to_xbar[0][0][0][0][0][0][0][0][0]
-    # lp = mapping_information.layer_mapping_to_pe[0][0][0][0][1]
-    # print(lp[0])
-    # exit()
 
     ### Scheduling ###
     print("Scheduling policy: ", end="")
@@ -64,40 +57,20 @@ def main():
     if not os.path.exists('./statistics/'+mapping_str+'/'+scheduling_str):
             os.makedirs('./statistics/'+mapping_str+'/'+scheduling_str)
     
-    ### Generate Order ###
-    isGenerateOrder = True
-
     ### Trace ###
     isTrace_order      = True
-    isTrace_controller = False
+    isTrace_controller = True
 
     ### Generate computation order graph ### 
-    if isGenerateOrder:
-        start_order_time = time.time()
-        print("--- Generate computation order graph ---")
-
-        order_generator = OrderGenerator(mapping_information, isTrace_order)
-
-        end_order_time = time.time()
-        print("--- Computation order graph is generated in %s seconds ---\n" % (end_order_time - start_order_time))
-
-    else:
-        print("Load order_generator from file \"ComputationOrder\"")
-        start_load = time.time()
-        with open ('ComputationOrder', 'rb') as fp:
-            order_generator = pickle.load(fp)
-        end_load = time.time()
-        print("--- Load computation order in %s seconds ---\n" %(end_load - start_load))
-
-    #exit()
-    ## delete mapping data
-    # del order_generator.mp_info
-    # del mapping_information
+    start_order_time = time.time()
+    print("--- Generate computation order graph ---")
+    order_generator = OrderGenerator(mapping_information, isTrace_order)
+    end_order_time = time.time()
+    print("--- Computation order graph is generated in %s seconds ---\n" % (end_order_time - start_order_time))
 
     ## Power and performance simulation ###
     start_simulation_time = time.time()
     print("--- Power and performance simulation---")
-
     controller = Controller(order_generator, isTrace_controller, mapping_str, scheduling_str)
     controller.run()
 
