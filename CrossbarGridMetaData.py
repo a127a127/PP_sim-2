322efdf10d0d8c17d7edbe087a14316f734b907a
wei-ting <r06944031@ntu.edu.tw>
2020-03-09 01:18:12 +0800

    刪除無用檔案

--- CrossbarGridMetaData.py
+++
@@ -1,9 +0,0 @@
-class CrossbarGridMetaData(object):
-    def __init__(self, nlayer, ngrid, nfilter, nbit):
-        self.nlayer = nlayer
-        self.nfilter = nfilter
-        self.ngrid = ngrid
-        self.nbit = nbit
-
-    def __str__(self):
-        return str(self.__dict__)
