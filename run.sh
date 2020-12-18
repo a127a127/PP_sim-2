5ea10a3d01dd4b4d8616a915862e3daad9b11114
wei-ting <r06944031@ntu.edu.tw>
2020-12-18 21:41:10 +0800

    script

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 model="Lenet"
-# model="Cifar10"
-# model="DeepID"
-# model="Caffenet"
-# model="Overfeat"
+#model="Cifar10"
+#model="DeepID"
+#model="Caffenet"
+#model="Overfeat"
 # model="VGG16"
-# model="Test"
+#model="Test"
+ 
+python3 PP_sim_o.py $model LIDR Non-pipeline 1 1 64
+python3 PP_sim_o.py $model LIDR Pipeline 1 1 64
+python3 PP_sim_o.py $model HIDR Non-pipeline 1 1 64
+python3 PP_sim_o.py $model HIDR Pipeline 1 1 64
 
-python3 PP_sim.py $model HIR Non-pipeline 1 1
-python3 PP_sim.py $model HIR Pipeline 1 1
-python3 PP_sim.py $model LIR Non-pipeline 1 1
-python3 PP_sim.py $model LIR Pipeline 1 1
+python3 PP_sim_o.py $model LIDR Non-pipeline 2 2 64
+python3 PP_sim_o.py $model LIDR Pipeline 2 2 64
+python3 PP_sim_o.py $model HIDR Non-pipeline 2 2 64
+python3 PP_sim_o.py $model HIDR Pipeline 2 2 64
 
-python3 PP_sim.py $model HIR Non-pipeline 2 2
-python3 PP_sim.py $model HIR Pipeline 2 2
-python3 PP_sim.py $model LIR Non-pipeline 2 2
-python3 PP_sim.py $model LIR Pipeline 2 2
-
-python3 PP_sim.py $model HIR Non-pipeline 4 4
-python3 PP_sim.py $model HIR Pipeline 4 4
-python3 PP_sim.py $model LIR Non-pipeline 4 4
-python3 PP_sim.py $model LIR Pipeline 4 4
+python3 PP_sim_o.py $model LIDR Non-pipeline 4 4 64
+python3 PP_sim_o.py $model LIDR Pipeline 4 4 64
+python3 PP_sim_o.py $model HIDR Non-pipeline 4 4 64
+python3 PP_sim_o.py $model HIDR Pipeline 4 4 64
 
